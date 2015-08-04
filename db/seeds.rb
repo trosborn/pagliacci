49b4753d7fa522395c6091b1729f0847e209c06f
Thomas Osborn <trosborn@gmail.com>
2015-08-04 14:12:06 -0700

    fix prices in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-50.times do MenuPizza.create(name: 'The Original', description: "Our signature cheese pizza made with whole-milk mozzarella and our seasoned tomato sauce on hand-tossed dough, baked on bricks.", small_price: "10.99", medium_price: "$14.99", large_price: "17.99")
+50.times do MenuPizza.create(name: 'The Original', description: "Our signature cheese pizza made with whole-milk mozzarella and our seasoned tomato sauce on hand-tossed dough, baked on bricks.", small_price: 10.99, medium_price: 14.99, large_price: 17.99)
 
 end
