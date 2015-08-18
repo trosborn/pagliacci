73e5ce734c33095fb1eb7503ccdcf5e4c0485049
trosborn <trosborn@gmail.com>
2015-08-17 23:25:14 -0700

    adds delivery hour variable to site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :delivery_hours
+
+  def delivery_hours
+    @delivery_hours = 'fuck it'
+  end
 end
