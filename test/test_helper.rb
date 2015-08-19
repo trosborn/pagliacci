639df005945c26bb73fbbfb074ab4b3eb0a8dbd0
Thomas Osborn <trosborn@gmail.com>
2015-08-19 00:18:23 -0700

    add coveralls to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,9 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/rails'
 require 'minitest/rails/capybara'
+require 'coveralls'
+
+Coveralls.wear!('rails')
 
 # To add Capybara feature tests add `gem 'minitest-rails-capybara'`
 # to the test group in the Gemfile and uncomment the following:
