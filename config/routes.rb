3f8b47dca04c47acce94edb7ade5e722c1b12e41
Thomas Osborn <trosborn@gmail.com>
2015-09-22 17:08:30 -0700

    finish admin crud of gelatos

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
+  get 'gelatos/new'
+
+  get 'gelatos/index'
+
+  get 'gelatos/create'
+
+  get 'gelatos/update'
+
+  get 'gelatos/destroy'
+
+  get 'gelatos/save'
+
   devise_for :users
   root 'welcome#index'
 
   resources :menu_pizzas
   resources :toppings
   resources :salads
+  resources :gelatos
 
   get 'welcome/about'
   get 'welcome/catering'
