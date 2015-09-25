fbbe86a98785bc8f4eed0f380556e61a800d50eb
Thomas Osborn <trosborn@gmail.com>
2015-09-24 21:30:57 -0700

    clean front-end and add order button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
 private
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
